ffbed472c86fcd7b82893f91d541ebbe1844c341
trustie_rity <trustiekiguru01@gmail.com>
2022-10-18 07:34:40 +0300

    add crypto category

---
+++ crypto/Encoding_challange/README.md
@@ -0,0 +1,5 @@
+### Encoding Challenge Writeup
+#### Cryptohack
+- Connect at ``nc socket.cryptohack.org 13377``
+- use python3 to run the script
+- `` python3 a.py ``
---
+++ crypto/Encoding_challange/a.py
@@ -0,0 +1,46 @@
+from pwn import * # pip install pwntools
+import json , subprocess , base64
+from Crypto.Util.number import *
+
+r = remote('socket.cryptohack.org', 13377, level = 'info')
+
+def json_recv():
+    line = r.recvline()
+    return json.loads(line.decode())
+
+def json_send(hsh):
+    request = json.dumps(hsh).encode()
+    r.sendline(request)
+
+while True:
+	try:
+		received = json_recv()
+        
+		print("Received type: ")
+		b = received["type"]
+		log.info(f"{b}")
+		print("Received encoded value: ")
+		c = received["encoded"]
+		log.info(f"{c}")
+        
+		if "utf-8" in b:
+			decoded = "".join([chr(i) for i in c])
+		elif "hex" in b:
+			decoded = bytes.fromhex(c).decode()
+		elif "rot13" in b:
+			decoded = subprocess.check_output(f"echo {c} | rot13",shell=True).strip().decode()
+		elif "bigint" in b:
+			#decoded = bytes.fromhex(c.strip("0x")).decode()
+			decoded = long_to_bytes(int(c.strip("0x"),16)).decode()
+		elif "base64" in b:
+			decoded = base64.b64decode(c).decode()
+		print(decoded)
+		to_send = {
+			"decoded": f"{decoded}"
+		}
+		json_send(to_send)
+        
+		#print(json_recv())
+	except:
+		print(received)
+		break	
